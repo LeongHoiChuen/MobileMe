4d313536bf3b8b63ef34724fabfd2e87355b39a8
LeongHoiChuen <hcleong.2012@sis.smu.edu.sg>
2015-08-09 16:56:46 +0800

    Login Module
    Login authentication (API calling) - Lacking a functional session
    creation - Testing whether it is functioning

--- app/src/main/java/com/example/jiabaotan2012/cw/MainMenuActivity.java
+++ app/src/main/java/com/example/jiabaotan2012/cw/MainMenuActivity.java
@@ -12,14 +12,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -30,26 +37,28 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class MainMenuActivity extends ActionBarActivity {
-    EditText ed1,ed2;
+    EditText ed1,ed2,resultText;
 
     TextView tx1;
     int counter = 3;
+    LoginUser loginUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        ed1 = (EditText)findViewById(R.id.editText);
+        ed2 = (EditText)findViewById(R.id.editText2);
+        resultText = (EditText) findViewById(R.id.resultText);
 
         final Button button = (Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent register = new Intent(view.getContext(), RegisterActivity.class);
-                startActivity(register);
+                //Intent register = new Intent(view.getContext(), RegisterActivity.class);
+                //startActivity(register);
+                new HttpAsyncTask().execute("https://clockwork-api.herokuapp.com/users/sign_in.json");
             }
         });
-
-        ed1 = (EditText)findViewById(R.id.editText);
-        ed2 = (EditText)findViewById(R.id.editText2);
 
         final Button button2 = (Button)findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
@@ -57,6 +66,15 @@ public class MainMenuActivity extends ActionBarActivity {
             public void onClick(View view) {
                 Intent jobListing = new Intent(view.getContext(), JobListsActivity.class);
                 startActivity(jobListing);
+            }
+        });
+
+        final Button button3 = (Button)findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent register = new Intent(view.getContext(), RegisterActivity.class);
+                startActivity(register);
             }
         });
 
@@ -84,5 +102,89 @@ public class MainMenuActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public static String POST(String url, LoginUser loginUser){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // 1. create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // 2. make POST request to the given URL
+            HttpPost httpPost = new HttpPost(url);
+
+            String json = "";
+
+            // 3. build jsonObject
+            JSONObject jsonObject = new JSONObject();
+
+            List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+
+            pairs.add(new BasicNameValuePair("user[username]", loginUser.getName()));
+            pairs.add(new BasicNameValuePair("user[password]", loginUser.getPassword()));
+
+            // 4. convert JSONObject to JSON to String
+            json = jsonObject.toString();
+
+            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+            // ObjectMapper mapper = new ObjectMapper();
+            // json = mapper.writeValueAsString(person);
+
+            // 5. set json to StringEntity
+            //StringEntity se = new StringEntity(json);
+
+            // 6. set httpPost Entity
+            httpPost.setEntity(new UrlEncodedFormEntity(pairs));
+
+            // 7. Set some headers to inform server about the type of the content
+            httpPost.setHeader("Accept", "application/json");
+
+            // 8. Execute POST request to the given URL
+            HttpResponse httpResponse = httpclient.execute(httpPost);
+
+            // 9. receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // 10. convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        // 11. return result
+        return result;
+    }
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            loginUser = new LoginUser(ed1.getText().toString(), ed2.getText().toString());
+
+            return POST(urls[0], loginUser);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Data Sent!", Toast.LENGTH_LONG).show();
+
+            resultText.setText(result);
+        }
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
     }
 }
